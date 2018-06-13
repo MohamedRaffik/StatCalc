c227fc3b8b97a0dfea66908a8b6afd1500e3e8b4
Mohamed Raffik <raffikmohamed7@gmail.com>
2018-06-13 00:09:06 -0400

    added navbar function

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-const liveServer = require('live-server');
-const stat = require('statistical-js');
-const jsdom = require('jsdom');
+const server = require('live-server');
 
-const { JSDOM } = jsdom;
-const { window } = new JSDOM('<DOCTYPE html>');
-const $ = require('jquery')(window);
-
-liveServer.start();
+server.start();
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-    console.log("heyyyaaa");
-})
+    var buttons = $(".navigation > a");
+    var displays = $(".mainDisplay > div");
+    var current_display = 0;
+
+    for (let i = 0; i < buttons.length; i++) {
+        $(buttons[i]).click(function() {
+            $(displays[current_display]).fadeOut(function() {
+                $(displays[i]).fadeIn();
+            });
+            current_display = i;
+        });
+    }
+
+    
+});
