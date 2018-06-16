0d3ab759af5dc9fcf8c30e4fa230e2bb3da15915
Mohamed Raffik <raffikmohamed7@gmail.com>
2018-06-16 15:15:38 -0400

    u3

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,7 +1,20 @@
+function GCalc (data) {
+    var str = data.split(",");
+    var num = str.map(n => parseInt(n));
+    const data = statistical.methods.summary(num);
+    const qrange = statistical.methods.interQuartileRange(num);
+    const svariance = statistical.methods.sampleVariance(num);
+    const sdev = statistical.methods.sampleStdDeviation(num);
+
+}
+
 $(document).ready(function() {
+    /** Navigation Bar click functions, displays the content of the selected
+      * Section by fading out the current content and fading in the new content
+     **/
     var current_display = 0;
-    $(".navigation > a").each(function(i, button) {
-        $(button).click(function() {
+    $(".navigation > a").each( (i, button) => {
+        $(button).click( () => {
             if (i == current_display) { return; }
             $($(".mainDisplay > div")[current_display]).fadeOut(function() {
                 $($(".mainDisplay > div")[i]).fadeIn();
@@ -9,4 +22,6 @@ $(document).ready(function() {
             current_display = i;
         });
     });
+
+    $("#generalCalcbutton").click( () =>{ GCalc($("textarea[name=gdata]").val()); } );
 });
